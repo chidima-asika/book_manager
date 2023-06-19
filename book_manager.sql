702f1bda8a68b421c3071d4391e6218547aa59dc
BlenWondimu <134359257+BlenWondimu@users.noreply.github.com>
2023-06-18 22:01:49 -0400

    Add files via upload
    entity tables
    relationship tables
    data dump

--- book_manager.sql
+++ book_manager.sql
@@ -1,48 +1,192 @@
-CREATE TABLE Book (
-    bookId INT PRIMARY KEY,
-    title VARCHAR(255),
-    numPages INT,
-    publicationYear INT,
-    numReviews INT,
-    averageRating DECIMAL(3,2)
+DROP DATABASE IF EXISTS BookManager;
+CREATE DATABASE IF NOT EXISTS BookManager;
+
+USE BookManager;
+
+
+CREATE TABLE user
+(
+	username VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    num_followers INT DEFAULT 0,
+    num_following INT DEFAULT 0
+
 );
 
-CREATE TABLE Author (
-    first_last_name VARCHAR(255) PRIMARY KEY,
-    numBooks INT
+CREATE TABLE librarian
+(
+	lib_username VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL
+
 );
 
-CREATE TABLE Genre (
-    name VARCHAR(255) PRIMARY KEY,
-    description TEXT,
-    numBooks INT
+
+CREATE TABLE book
+(
+	bookId INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    num_pages INT NOT NULL,
+    publication_year YEAR NOT NULL,
+    librarian_username VARCHAR(30),
+
+	FOREIGN KEY (librarian_username) REFERENCES librarian (lib_username)
 );
 
-CREATE TABLE Review (
-    reviewId INT PRIMARY KEY,
-    rating INt(3,2), 
-    description TEXT
+
+CREATE TABLE genre
+(
+	name VARCHAR(100) PRIMARY KEY,
+    description VARCHAR(500) DEFAULT NULL,
+	num_books INT DEFAULT 0
+
 );
 
-CREATE TABLE BookClub (
-    name VARCHAR(255) PRIMARY KEY,
-    active BOOLEAN,
-    numMembers INT
+
+CREATE TABLE author
+(
+	first_last_name VARCHAR(100) PRIMARY KEY,
+	num_books INT DEFAULT 0
+    
 );
 
-CREATE TABLE User (
-    username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255),
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    numFollowers INT,
-    numFollowing INT,
-    status VARCHAR(255)
+
+CREATE TABLE reviews
+(
+	reviewId INT AUTO_INCREMENT PRIMARY KEY,
+    rating INT NOT NULL,
+    description VARCHAR(500) DEFAULT NULL,
+    num_reviews INT DEFAULT 0
 );
 
-CREATE TABLE Librarian (
-    username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255),
-    firstName VARCHAR(255),
-    lastName VARCHAR(255)
+
+CREATE TABLE book_club
+(
+
+    club_name VARCHAR(100) PRIMARY KEY,
+    active TINYINT(1) DEFAULT 1,
+    bookId INT,
+    librarian VARCHAR(30),
+    num_members INT DEFAULT 1,
+
+	FOREIGN KEY (bookId) REFERENCES book (bookId),
+    FOREIGN KEY (librarian) REFERENCES librarian (lib_username)
+    
 );
+
+
+CREATE TABLE book_user
+(
+	bookId INT,
+    username VARCHAR(30),
+    status VARCHAR(30) NOT NULL,
+    
+    FOREIGN KEY (bookId) REFERENCES book (bookId),
+    FOREIGN KEY (username) REFERENCES user (username)
+    
+);
+
+
+CREATE TABLE book_genre
+(
+	bookId INT,
+    genre_name VARCHAR(100),
+    
+    FOREIGN KEY (bookId) REFERENCES book (bookId),
+    FOREIGN KEY (genre_name) REFERENCES genre (name)
+    
+);
+
+
+CREATE TABLE book_author
+(
+	bookId INT,
+    author_name VARCHAR(100),
+    
+    FOREIGN KEY (bookId) REFERENCES book (bookId),
+    FOREIGN KEY (author_name) REFERENCES author (first_last_name)
+    
+);
+
+
+CREATE TABLE book_club_members
+(
+	club_name VARCHAR(100),
+    member VARCHAR(30),
+    
+    FOREIGN KEY (club_name) REFERENCES book_club (club_name),
+    FOREIGN KEY (member) REFERENCES user (username)
+    
+);
+
+
+CREATE TABLE user_follows_user
+(
+	username VARCHAR(30),
+    following_username VARCHAR(30),
+    
+    FOREIGN KEY (username) REFERENCES user (username),
+    FOREIGN KEY (following_username) REFERENCES user (username)
+    
+);
+
+
+CREATE TABLE user_review_book
+(
+	bookId INT,
+    username VARCHAR(30),
+    reviewId INT,
+    
+    FOREIGN KEY (bookId) REFERENCES book (bookId),
+    FOREIGN KEY (username) REFERENCES user (username),
+    FOREIGN KEY (reviewId) REFERENCES reviews (reviewId)
+    
+);
+
+
+# Data dump
+
+INSERT INTO librarian (lib_username, password, first_name, last_name) VALUES
+('test_librarian', 'tester123', 'Test', 'Librarian');
+
+INSERT INTO user (username, password, first_name, last_name) VALUES
+('test_user', 'tester456', 'Test', 'User'),
+('jane_doe_22', 'doremefaso', 'Jane', 'Doe');
+
+INSERT INTO book (title, num_pages, publication_year, librarian_username) VALUES 
+('The Great Gatsby', 180, 1925, 'test_librarian'),
+('To Kill a Mocking Bird', 323, 1960, 'test_librarian'),
+('1984', 368, 1949, 'test_librarian'),
+('Harry Potter and the Deathly Hallows', 759, 2007, 'test_librarian');
+
+INSERT INTO author VALUES 
+('F. Scott Fitzgerald'), 
+('Harper Lee'), 
+('George Orwell'),
+('J.K. Rowling');
+
+INSERT INTO book_author (bookId, author_name) VALUES
+(1, 'F. Scott Fitzgerald'),
+(2, 'Harper Lee'),
+(3, 'George Orwell'),
+('4', 'J.K. Rowling');
+
+INSERT INTO genre (name) VALUES
+('Fiction'), ('Historical Fiction'), ('Science Fiction'), ('Fantasy');
+
+INSERT INTO book_genre (bookId, genre_name) VALUES
+(1, 'Fiction'), (1, 'Historical Fiction'),
+(2, 'Fiction'), (2, 'Historical Fiction'),
+(3, 'Fiction'), (3, 'Science Fiction'),
+(4, 'Fantasy');
+
+INSERT INTO book_club (club_name, bookId, librarian) VALUES
+('PotterHeads', 4, 'test_librarian'),
+('Classics Lovers', 1, 'test_librarian');
+
+INSERT INTO book_club_members (club_name, member) VALUES
+('PotterHeads', 'test_user'),
+('Classics Lovers', 'jane_doe_22');
